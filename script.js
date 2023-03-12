e6d2bc153c7b7896c8913f548250b6f5fad24be1
Stas-Mykhailenko <stas1110900011>
2023-03-12 21:55:00 +0300

    format code

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-let a='';
-for( let i = 10; i <20; i++) a+= i + ',';
+let a = '';
+for(let i = 10; i < 20; i++) a += i + ',';
 console.log(a);
 
-let b='';
-for( let i = 15; i <35; i++){
-b+=i+','
+let b = '';
+for(let i = 15; i < 35; i++) {
+b += i + ','
 Math.pow(i, 2)};
 console.log(b);
 
-let sum=0;
-let count=0
-for(let i=1;  i<500; i++){
-	sum+=i
-	count+=1
+let sum = 0;
+let count = 0
+for(let i = 1; i < 500; i++) {
+	sum += i
+	count += 1
 };
-
 console.log(sum/count);
-let summa=0;
-for(let i=30; i<80; i++){
-	if(i%2 === 0);
-	summa +=i	
-}
 
-console.log(summa);
-for(let i=100; i<300; i++){
-	if(i%3 ===0)
+let result = 0;
+for(let i = 30; i<80; i++) {
+	if(i % 2 === 0) result += i	
+}
+console.log(result);
+
+for(let i = 100; i < 300; i++) {
+	if(i % 3 === 0)
 	console.log(i)
 }
 
 let n=prompt('Write number');
-	for(let i=2; i<n; i++){
-		if(n%i===0){console.log('false number')
-	}else{console.log('Try number')} break
-	};
+for(let i=2; i<n; i++) {
+	if(n % i === 0){
+		console.log('false number')
+	} else {
+		console.log('Try number')
+	} 
+};
