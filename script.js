77677e87f8b96655b905e13ac27ec0e126f0cd82
Stas-Mykhailenko <stas1110900011>
2023-03-13 22:41:15 +0300

    add jne task

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 let a = '';
-for(let i = 10; i < 20; i++) {
+for(let i = 10; i <= 20; i++) {
 	a += i + ',';
 };
 console.log(a);
 
 let b = '';
-for(let i = 15; i < 35; i++) {	
+for(let i = 15; i <= 35; i++) {	
 	b += Math.pow(i, 2) + ','
 };
 console.log(b);
 
+let x = 1;
+for(let i = 15; i <= 35; i++) {
+	x *= i;
+};
+console.log(x);
+
+
 let sum = 0;
 let count = 0
-for(let i = 1; i < 500; i++) {
+for(let i = 1; i <= 500; i++) {
 	sum += i
 	count += 1
 };
 console.log(sum/count);
 
 let result = 0;
-for(let i = 30; i < 80; i++) {
+for(let i = 30; i <= 80; i++) {
 	if(i % 2 === 0) result += i	
 }
 console.log(result);
 
-for(let i = 100; i < 300; i++) {
+for(let i = 100; i <= 300; i++) {
 	if(i % 2 !== 0)
 	console.log(i)
 }
