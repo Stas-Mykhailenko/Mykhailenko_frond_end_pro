f5642504a2cb6ff5bcc721a18dd7fc11cbfb9cfc
Stas-Mykhailenko <stas1110900011>
2023-03-30 21:17:05 +0200

    first commit

---
+++ function.js
@@ -0,0 +1,23 @@
+// 1)Написать функцию которая будет возвращать уникальные значения в массиве
+
+// Пример:
+
+// const yourFunc = (arr) => {};
+
+// const arr1 = [1,2,3,4,5,1,2,3,4,5]
+
+// yourFunc(arr1) // [1,2,3,4,5]
+
+// Должно работать с любыми значениями
+// const yourFunc = (arr) =>{
+// 	let arr = arr.reduce((fistItem, item) =>{
+
+// 	})
+// }
+
+	
+const arr1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+// yourFunc(arr1); // [1,2,3,4,5]
+// console.log(arr1);
+let array = [];
+console.log(array[0])
