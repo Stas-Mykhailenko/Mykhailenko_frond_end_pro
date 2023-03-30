3684f0ce72ad236008d3c8b2a6af03da9b393ab6
Stas-Mykhailenko <stas1110900011>
2023-03-30 21:43:29 +0200

    First task

--- function.js
+++ function.js
@@ -9,15 +9,17 @@
 // yourFunc(arr1) // [1,2,3,4,5]
 
 // Должно работать с любыми значениями
-// const yourFunc = (arr) =>{
-// 	let arr = arr.reduce((fistItem, item) =>{
-
-// 	})
-// }
-
-	
+const yourFunc = (arr) =>{
+	let array = [];
+	array.push(arr[0]);
+	console.log(array)
+	arr.forEach(element => {
+		if(array.includes(element) !== array.includes(element))
+		array.push(element)
+		console.log (element)
+	});
+	return array
+}
 const arr1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
-// yourFunc(arr1); // [1,2,3,4,5]
-// console.log(arr1);
-let array = [];
-console.log(array[0])
+yourFunc(arr1); // [1,2,3,4,5]
+console.log(yourFunc(arr1));
