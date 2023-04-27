e16775e4d3fc64c4608a271fa7e0a0717f6242d9
Stas-Mykhailenko <stas1110900011>
2023-04-27 21:32:21 +0200

    Last commit

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const SIZE_SMALL = "small";
-const SIZE_BIG = "big";
+const SIZE_SMALL = {
+  name: "small",
+  price: 50,
+  cal: 20,
+};
+const SIZE_BIG = {
+  name: "big",
+  price: 100,
+  cal: 40,
+};
 
-const STUFFING_CHEESE = "cheese";
-const STUFFING_SALAD = "salad";
-const STUFFING_POTATO = "potato";
+const STUFFING_CHEESE = {
+  name: "cheese",
+  price: 100,
+  cal: 40,
+};
+const STUFFING_SALAD = {
+  name: "salad",
+  price: 100,
+  cal: 40,
+};
+const STUFFING_POTATO = {
+  name: "potato",
+  price: 100,
+  cal: 40,
+};
 
 const TOPPING_MAYO = "mayo";
 const TOPPING_SAUCE = "sauce";
@@ -12,90 +32,60 @@ class Hamburger {
   constructor(size, stuffing) {
     this.size = size;
     this.stuffing = stuffing;
-    this.cal = 0;
-    this.price = 0;
-    this.order = {
+    this.cal = this.size.cal + this.stuffing.cal;
+    this.price = this.size.price + this.stuffing.price;
+    this.listTopping = {
       mayo: 0,
       sauce: 0,
     };
   }
-  //
+  calories() {
+    return;
+  }
   addtopping(topping) {
     if (topping === "mayo") {
       this.cal += 5;
       this.price += 20;
-      this.order[topping]++;
+      this.listTopping[topping]++;
     }
     if (topping === "sauce") {
       this.price += 15;
-      this.order[topping]++;
+      this.listTopping[topping]++;
     }
     return this;
   }
   //
 
   deletetopping(topping) {
-    if (this.order["mayo"] !== 0 && topping === "mayo") {
+    if (this.listTopping["mayo"] !== 0 && topping === "mayo") {
       this.cal -= 5;
       this.price -= 20;
       this.order[topping]--;
     }
-    if (this.order["sauce"] !== 0 && topping === "sauce") {
+    if (this.listTopping["sauce"] !== 0 && topping === "sauce") {
       this.price -= 15;
-      this.order[topping]--;
+      this.listTopping[topping]--;
     }
     return this;
   }
-  showorder() {
-    if (this.size === "small") {
-      this.cal += 20;
-      this.price += 50;
-      switch (this.stuffing) {
-        case STUFFING_CHEESE:
-          this.cal += 20;
-          this.price += 10;
-          break;
-        case STUFFING_SALAD:
-          this.cal += 5;
-          this.price += 20;
-          break;
-        case STUFFING_POTATO:
-          this.cal += 10;
-          this.price += 15;
-          break;
-      }
-    }
-    if (this.size === "big") {
-      this.cal += 40;
-      this.price += 100;
-      switch (this.stuffing) {
-        case STUFFING_CHEESE:
-          this.cal += 20;
-          this.price += 10;
-          break;
-        case STUFFING_SALAD:
-          this.cal += 5;
-          this.price += 20;
-          break;
-        case STUFFING_POTATO:
-          this.cal += 10;
-          this.price += 15;
-          break;
-      }
-    }
-    let order = {
-      nameSandwich: "Humburger",
-      stuffing: this.stuffing,
-      topping: this.order,
-      calories: this.cal,
-      price: this.price,
-    };
-    return order;
+  showOrder() {
+    console.log(
+      "Размер гамбургера " + this.size.name,
+      "Начинка " + this.stuffing.name,
+      "Каллорийность " + this.cal,
+      "Цена " + this.price
+    );
+  }
+  showCalories() {
+    console.log("Каллорийность " + this.cal);
+  }
+  showPrice() {
+    console.log("Цена " + this.price);
   }
 }
 
 let hamburger = new Hamburger(SIZE_SMALL, STUFFING_CHEESE);
-hamburger.addtopping(TOPPING_MAYO)
-  .addtopping(TOPPING_MAYO)
-  .deletetopping(TOPPING_SAUCE);
-console.log(hamburger.showorder());
+// hamburger.addtopping(TOPPING_MAYO)
+//   .addtopping(TOPPING_MAYO)
+//   .deletetopping(TOPPING_SAUCE);
+hamburger.showOrder();
