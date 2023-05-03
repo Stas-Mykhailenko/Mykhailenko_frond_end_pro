fefb69aa778464264c50015ab88bb3b86e65a188
Stas-Mykhailenko <stas1110900011>
2023-05-03 20:15:54 +0200

    add smoe function

--- mainJs.js
+++ mainJs.js
@@ -1,8 +1,15 @@
 // main.js
-import { mod1Function } from './mod1.js'
+import { mod1Function,arr } from './mod1.js'
 
 const testFunction = () => {
     console.log('Im the main function')
     mod1Function()
 }
+let num =[1,2,3,4]
+function tes1(item){
+	let result = arr(item)
+	return result
+}
+
 testFunction()
+console.log(tes1(num))
--- mod1.js
+++ mod1.js
@@ -1,3 +1,11 @@
 // mod1.js
-const mod1Function = () => console.log('Mod1 is alive!')
-export {mod1Function }
+const mod1Function = () => console.log("Mod1 is alive!");
+// // // // // // // // //
+function arr(item) {
+  let count;
+  item.forEach((element) => {
+    count =element + 1;
+  });
+  return count;
+}
+export { mod1Function, arr };
