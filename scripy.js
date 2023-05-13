348d3dfc96f2b45a48dac321fc0f63b69d12e767
Stas-Mykhailenko <stas1110900011>
2023-05-13 19:13:30 +0200

    first commit

---
+++ scripy.js
@@ -0,0 +1,20 @@
+const conteiner = document.querySelector(".conteiner");
+const slideleft = document.querySelector("#slideleft");
+const slideright = document.querySelector("#slideright");
+let positionleft = 0;
+slideright.addEventListener("click", () => {
+  if (positionleft !== -2450) {
+    positionleft -= 350;
+    conteiner.style.left = positionleft + "px";
+  }
+  if (positionleft === -2450) {
+    slideright.style.display = "none";
+  }
+});
+slideleft.addEventListener("click", () => {
+  if (positionleft !== 0) {
+    positionleft += 350;
+    conteiner.style.left = positionleft + "px";
+    slideright.style.display = "inline";
+  }
+});
