ffdfa0959f56c690079ef177bdc639129a2f61e6
Stas-Mykhailenko <stas1110900011>
2023-05-15 20:04:01 +0200

    finaly task

--- script.js
+++ script.js
@@ -10,20 +10,32 @@ const productbutton4 = document.querySelector(".productbutton4");
 const buyinfo = document.querySelector(".buyinfo");
 const form = document.querySelector(".form");
 const full_name = document.querySelector("#full_name");
+const full_namecolor = document.querySelector(".full_namecolor");
 const city = document.querySelector("#city");
 const post_office1 = document.querySelector("#post_office1");
 const post_office2 = document.querySelector("#post_office2");
 const post_office3 = document.querySelector("#post_office3");
 const payment_card = document.querySelector("#payment_card");
 const payment_card2 = document.querySelector("#payment_card2");
+const colorradio_first = document.querySelector(".colorradio_first");
+const colorradio_second = document.querySelector(".colorradio_second");
 const quantity = document.querySelector("#quantity");
+const quantity_color = document.querySelector(".quantity_color");
 const error = document.querySelector("#error");
 const buttonreg = document.querySelector("#buttonreg");
-const attention =document.querySelector(".attention");
-let nameInput=null;
-let quantityInput=null;
-let choisePay=null;
-
+const attention = document.querySelector(".attention");
+const listorder = document.querySelector(".listorder");
+const firsttab = document.querySelector("#firsttab");
+const secondtab = document.querySelector("#secondtab");
+const thirdtab = document.querySelector("#thirdtab");
+const fouthtab = document.querySelector("#fouthtab");
+const fifthtab = document.querySelector("#fifthtab");
+const listorder_button1 = document.querySelector("#listorder_button1");
+const listorder_button2 = document.querySelector("#listorder_button2");
+let nameInput = null;
+let quantityInput = null;
+let choisePay = null;
+let post_office = post_office1.value;
 jameson.addEventListener("click", () => {
   dropdown.style.display = "none";
   product2.style.display = "inline-block";
@@ -71,6 +83,15 @@ city.addEventListener("change", () => {
     post_office3.style.display = "block";
   }
 });
+post_office1.addEventListener("click", () => {
+  post_office = post_office1.value;
+});
+post_office2.addEventListener("click", () => {
+  post_office = post_office2.value;
+});
+post_office3.addEventListener("click", () => {
+  post_office = post_office3.value;
+});
 payment_card.addEventListener("click", () => {
   choisePay = payment_card.value;
 });
@@ -79,28 +100,51 @@ payment_card2.addEventListener("click", () => {
 });
 
 quantity.addEventListener("input", () => {
-
   if (!isNaN(quantity.value)) {
     quantityInput = quantity.value;
     error.style.display = "none";
   } else {
     error.style.display = "block";
-   
+  }
+});
+buttonreg.addEventListener("click", () => {
+  if (nameInput === null) {
+    full_namecolor.style.color = "#ff0000";
+  }
+});
+buttonreg.addEventListener("click", () => {
+  if (quantityInput === null) {
+    quantity_color.style.color = "#ff0000";
   }
 });
 
 buttonreg.addEventListener("click", () => {
-  if( quantityInput !== null && nameInput !== null && choisePay !== null){
-    console.log(quantityInput)
-    console.log(nameInput)
-    form.style.display = "none";
-    buyinfo.style.display = "inline-block";
-    attention.style.display = "none"
-    setTimeout(() => {
-      buyinfo.style.display = "none";
-      dropdown.style.display = "flex";
-    }, 3000);
-  }else{attention.style.display = "block"}
- 
+  if (choisePay === null) {
+    colorradio_second.style.color = "#ff0000";
+    colorradio_first.style.color = "#ff0000";
+  }
 });
-
+buttonreg.addEventListener("click", () => {
+  if (quantityInput !== null && nameInput !== null && choisePay !== null) {
+    form.style.display = "none";
+    listorder.style.display = "block";
+    attention.style.display = "none";
+  }
+  firsttab.innerHTML = `ФИО ${nameInput}`;
+  secondtab.innerHTML = `Місто ${city.value}`;
+  thirdtab.innerHTML = `Відділеня НП  ${post_office}`;
+  fouthtab.innerHTML = `Спосіб оплати ${choisePay}`;
+  fifthtab.innerHTML = `Кількість     ${quantityInput} шт.`;
+});
+listorder_button1.addEventListener("click", () => {
+  listorder.style.display = "none";
+  buyinfo.style.display = "block";
+  setTimeout(() => {
+    buyinfo.style.display = "none";
+    dropdown.style.display = "flex";
+  }, 3000);
+});
+listorder_button2.addEventListener("click", () => {
+  listorder.style.display = "none";
+  form.style.display = "flex";
+});
